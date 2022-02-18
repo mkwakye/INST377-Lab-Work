7d4f048b1be5be00cd96e6393cd51fa7b61d7050
tokyodrift7 <mkwakye99@gmail.com>
2022-02-18 13:54:56 -0600

    Updated script.js to include shape for L Tetromino

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -4,4 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const ScoreDisplay = document.querySelector('#score')
   const StartBtn = document.querySelector('#start-button')
   const width = 10
+
+  // Tetros
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2]
+  ]
 })
