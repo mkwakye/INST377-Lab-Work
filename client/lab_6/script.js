c19316948dd6128bc908f956b5fdb96f070ea57d
tokyodrift7 <mkwakye99@gmail.com>
2022-03-12 23:57:59 -0600

    Copied code from lab 5
    Copied code for index.html, script.js and styles.css. Made new async function dataHandler() and tweaks to manEvent()

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,15 +1,43 @@
+async function dataHandler(pgArray) {
+  console.table(pgArray);
+}
+
+async function mainEvent() {
+  console.log('script loaded');
+  const form = document.querySelector('.main_form');
+  const but = document.querySelector('.button');
+  but.style.display = 'none';
+
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+  const arrayFromJson = await results.json();
+
+  if (arrayFromJson.data.length > 0) {
+    form.addEventListener('submit', async (submitEvent) => {
+      but.style.display = 'block';
+      submitEvent.preventDefault();
+      console.log('form submission');
+      dataHandler(arrayFromJson.data);
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', async () => dataHandler());
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+
+/*
+
 async function mainEvent() { // the async keyword means we can make API requests
   const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+  form.addEventListener('submit', async (submitEvent) => {
+    // async has to be declared all the way to get an await
     submitEvent.preventDefault(); // This prevents your page from refreshing!
     console.log('form submission'); // this is substituting for a "breakpoint"
     const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
     const arrayFromJson = await results.json(); // This changes it into data we can use - an object
     console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
   });
 }
 
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
+// the async keyword means we can make API requests
+*/
