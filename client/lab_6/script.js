629562e3a9cd00d878bc71e7e07391ee066d3887
tokyodrift7 <mkwakye99@gmail.com>
2022-03-13 19:32:48 -0500

    Added new functions to script.js; New classes to styles.css

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,54 +1,48 @@
-async function dataHandler(dataArray) {
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1)) + newMin;
+}
+
+function restoArrayMake(dataArray) {
   console.log('fired dataHandler');
   console.table(dataArray);
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
     const restNum = getRandomIntInclusive(0, dataArray.length - 1);
-
-    return;
-  })
-
-  range.forEach((item) => {
-    console.log('range item', item);
+    return dataArray[restNum];
   });
+  console.log(listItems);
+  return listItems;
+
+  // range.forEach((item) => {
+  //   console.log('range item', item);
+  // });
+}
+
+function createHtmlList(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
 }
 
 async function mainEvent() {
   console.log('script loaded');
-  const form = document.querySelector('.main_form');
+  const form = document.querySelector('.page_form');
   const but = document.querySelector('.button');
   but.style.display = 'none';
 
   const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   const arrayFromJson = await results.json();
+  console.log(arrayFromJson);
 
   if (arrayFromJson.length > 0) {
+    but.style.display = 'block';
     form.addEventListener('submit', async (submitEvent) => {
-      but.style.display = 'block';
       submitEvent.preventDefault();
       console.log('form submission');
-      dataHandler(arrayFromJson);
+      const restoArray = restoArrayMake(arrayFromJson);
+      createHtmlList(restoArray);
     });
   }
 }
-document.addEventListener('DOMContentLoaded', async () => dataHandler());
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
-
-/*
-
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form');
-  form.addEventListener('submit', async (submitEvent) => {
-    // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-  });
-}
-
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
-// the async keyword means we can make API requests
-*/
