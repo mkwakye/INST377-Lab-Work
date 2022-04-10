bf29f92085f50a693c31080a40a14170f6c6a05d
tokyodrift7 <mkwakye99@gmail.com>
2022-04-10 15:16:13 -0500

    Update script.js

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -35,7 +35,7 @@ function getRandomIntInclusive(min, max) {
   }
 
   function initMap(targetId) {
-    const latLong = [38.7849, 76.8721];
+    const latLong = [38.7849, -76.8721];
     const map = L.map(targetId).setView(latLong, 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
